07a6acf01284f47c67b6499236373904edfb5d7f
wtao <wtao.jyg@qq.com>
2019-03-20 15:18:14 +0800

    添加了 HomeController

--- app/src/main/java/com/newer/mongo/AppApplication.java
+++ app/src/main/java/com/newer/mongo/AppApplication.java
@@ -3,6 +3,11 @@ package com.newer.mongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author wtao
+ *
+ */
 @SpringBootApplication
 public class AppApplication {
 
