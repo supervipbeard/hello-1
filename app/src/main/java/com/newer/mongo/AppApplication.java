f0f9799ac088e0c21f52d2e427b8519513a65ec1
wtao <wtao.jyg@qq.com>
2019-03-20 15:23:04 +0800

    修改了 HomeController

--- app/src/main/java/com/newer/mongo/AppApplication.java
+++ app/src/main/java/com/newer/mongo/AppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 
- * @author wtao
+ * @author root
  *
  */
 @SpringBootApplication
--- app/src/main/java/com/newer/mongo/HomeController.java
+++ app/src/main/java/com/newer/mongo/HomeController.java
@@ -1,8 +1,23 @@
 package com.newer.mongo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 
+ * @author wtao
+ *
+ */
 @RestController
 public class HomeController {
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
 
 }
